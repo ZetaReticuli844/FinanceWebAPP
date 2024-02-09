1bfba52ba9f40fd655aae8b55b863394aa275810
Yogesh <awesomeyogi10@gmail.com>
2024-02-10 00:13:14 +0530

    created models for user,income,expense and category

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Income,Expense,Category
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Income)
+admin.site.register(Expense)
--- backend/base/models.py
+++ backend/base/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
+from user.models import NewUser
 
 class Income(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(NewUser, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
     category = models.CharField(max_length=100)
 
 class Expense(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(NewUser, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
     category = models.CharField(max_length=100)
