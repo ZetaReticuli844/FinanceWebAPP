e967e6154e65757e7ab1b69f1ccb35ccbfe93132
Yogesh <awesomeyogi10@gmail.com>
2024-02-26 15:14:09 +0530

    remove venv file

--- frontend/src/pages/Welcome.tsx
+++ frontend/src/pages/Welcome.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 const Welcome = () => {
   const navigate=useNavigate()
-  React.useEffect(()=>{
+React.useEffect(()=>{
 navigate('/login')
   })
 }
